dd9b8ccde40ead21db489f18e36e74651c356244
Chris Nevin <cjnevin@gmail.com>
2016-06-26 08:45:35 +0100

    Converted to framework.

---
+++ AnagramDictionary/AnagramDictionary.h
@@ -0,0 +1,19 @@
+//
+//  AnagramDictionary.h
+//  AnagramDictionary
+//
+//  Created by Chris Nevin on 26/06/2016.
+//  Copyright © 2016 CJNevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnagramDictionary.
+FOUNDATION_EXPORT double AnagramDictionaryVersionNumber;
+
+//! Project version string for AnagramDictionary.
+FOUNDATION_EXPORT const unsigned char AnagramDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnagramDictionary/PublicHeader.h>
+
+
